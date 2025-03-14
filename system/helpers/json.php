9d6eec16bd908844725405538da2ac1b6a175a13
Julius Derigs <julius.derigs@mail.de>
2025-03-14 22:05:19 +0100

    Erweitern des JSON-Helpers.

--- system/helpers/json.php
+++ system/helpers/json.php
@@ -1,5 +1,7 @@
 <?php
 
+use JetBrains\PhpStorm\NoReturn;
+
 if (!function_exists('array_to_json')) {
     function array_to_json(array $array) :string {
         try {
@@ -27,5 +29,50 @@ if (!function_exists('json_to_array')) {
 
             return [];
         }
+    }
+}
+
+if (!function_exists('respond_with_json')) {
+    function respond_with_json(int $statuscode, string $json) :void {
+        http_response_code($statuscode);
+        header('Content-Type: application/json');
+        echo $json;
+    }
+}
+
+if (!function_exists('respond_with_500')) {
+    #[NoReturn] function respond_with_500(): void {
+        respond_with_json(500, array_to_json([
+            'success' => false,
+            'message' => esc(LANG['messages']['errors']['unknown_error']),
+            'data' => []
+        ]));
+
+        exit;
+    }
+}
+
+if (!function_exists('respond_with_400')) {
+    #[NoReturn] function respond_with_400(string $message, array $data = []) :void {
+        respond_with_json(400, array_to_json([
+            'success' => true,
+            'message' => esc($message),
+            'data' => $data
+        ]));
+
+        exit;
+    }
+}
+
+
+if (!function_exists('respond_with_200')) {
+    #[NoReturn] function respond_with_200(string $message, array $data = []) :void {
+        respond_with_json(200, array_to_json([
+            'success' => true,
+            'message' => esc($message),
+            'data' => $data
+        ]));
+
+        exit;
     }
 }
