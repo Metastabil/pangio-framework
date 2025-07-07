d01252919f610e83ced5fc8e15d5dbbf4a1e597f
Julius Derigs <julius.derigs@mail.de>
2025-07-07 12:15:38 +0200

    Umstellung der Sprachdatei auf YAML. Entfernen das Form-Helpers und Umstellung auf Request-Class.

--- system/helpers/language.php
+++ system/helpers/language.php
@@ -1,13 +1,18 @@
 <?php
 
+use Symfony\Component\Yaml\Yaml;
+
 if (!function_exists('get_language')) {
-    function get_language(string $locale = 'de') :array {
-        $filepath = dirname(__DIR__, 2) . '/languages/' . $locale . '.php';
+    function get_language(string $locale = 'de'): object {
+        $filepath = dirname(__DIR__, 2) . '/languages/' . $locale . '.yaml';
 
         if (!is_file($filepath)) {
             die("Missing language file: $filepath");
         }
 
-        return require $filepath;
+        // Parse the YAML file and convert the array to an object
+        $languageArray = Yaml::parseFile($filepath);
+
+        return json_decode(json_encode($languageArray, JSON_THROW_ON_ERROR), false, 512, JSON_THROW_ON_ERROR);
     }
 }
